1935d924f0809ce081f420b7810aecf48fddbd18
Sergio Alapont <seralgrainf@gmail.com>
2017-06-11 16:19:49 +0100

    Adding UnitTest support and modified UniTests project to use the "catch" library Created Basic project to test the progression of the engine

---
+++ Samples/UnitTests/src/UnitTests.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "../External/catch/catch.hpp"
--- build_samples_project.lua
+++ build_samples_project.lua
@@ -14,10 +14,7 @@ solution "TestingFenix"
 
 project "UnitTests"
     location "./Samples/UnitTests"
-    --kind "ConsoleApp"
-    --In case of using WindowedApp we have to specify te entry point
-    kind "WindowedApp"
-    flags { "WinMain" }
+    kind "ConsoleApp"
 
     targetdir "./Samples/UnitTests/build/$(Configuration)/$(Platform)"
     objdir "./Samples/UnitTests/obj/$(Platform)"
@@ -32,6 +29,67 @@ project "UnitTests"
     }
     files {
         "./Samples/UnitTests/src/**"     
+    }
+    libdirs 
+    { 
+        "./FenixDK/build/$(Configuration)/$(Platform)",
+    }
+    links {"FenixDK"}
+
+    configuration "Release"
+        defines { "_NDEBUG" }
+        flags { "OptimizeSpeed" }
+
+    configuration "Debug" 
+        defines { "_DEBUG" }
+        flags {"Symbols"}
+
+
+    configuration "x32"
+        libdirs { 
+            "C:/VulkanSDK/1.0.39.1/Bin32",
+        }
+        links {"vulkan-1"}
+        defines 
+        {
+            "Win32", 
+            "VK_PROTOTYPES",
+            "VK_USE_PLATFORM_WIN32_KHR",
+        }
+
+    configuration "x64"
+        libdirs { 
+            "C:/VulkanSDK/1.0.39.1/Bin",
+        }
+        links {"vulkan-1"}      
+        defines 
+        {
+            "x64", 
+            "VK_PROTOTYPES",
+            "VK_USE_PLATFORM_WIN32_KHR",
+        }
+
+
+project "Basic"
+    location "./Samples/Basic"
+    --kind "ConsoleApp"
+    --In case of using WindowedApp we have to specify te entry point
+    kind "WindowedApp"
+    flags { "WinMain" }
+
+    targetdir "./Samples/Basic/build/$(Configuration)/$(Platform)"
+    objdir "./Samples/Basic/obj/$(Platform)"
+    debugdir "./Samples/Res"
+    prebuildcommands { "call ../../scripts/compile_engine.bat $(Platform) $(Configuration) ../../FenixDK/FenixEngine.sln" }
+
+    includedirs {
+        "C:/VulkanSDK/1.0.39.1/Include",
+        "FenixDK/src",
+        "External/",
+        "./Samples/Basic/src" 
+    }
+    files {
+        "./Samples/Basic/src/**"     
     }
     libdirs 
     { 

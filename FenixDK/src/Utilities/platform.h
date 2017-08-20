5dfb1a01fc5fa153edf0146be416d1e4f620f326
Sergio Alapont <seralgrainf@gmail.com>
2017-08-20 17:52:00 +0100

    Removed useless macros

--- FenixDK/src/Utilities/platform.h
+++ FenixDK/src/Utilities/platform.h
@@ -29,16 +29,9 @@ constexpr u32 kEngineVersion = VK_MAKE_VERSION(0, 0, 1);
 
 #define IMPLEMENTATION_TYPE(API, ClassName) API##ClassName
 #define IMPL_CONCAT(API, ClassName) IMPLEMENTATION_TYPE(API, ClassName)
-#define CLASS_PTR(ClassName) ClassName##*
 
-// Used to get the name of the class that implements internally the specified class
-#define IMPL_NAME(ClassName) IMPL_CONCAT(GPUIMPL_ID, ClassName)
-// Get the name of a pointer to the class that implements internally the specified class
-#define IMPL_PTR(ClassName) CLASS_PTR(IMPL_NAME(ClassName))
-
-//#define IMPLEMENTATION(ClassName, pObj) reinterpret_cast<IMPL_PTR(ClassName)>(pObj)
+// Define used to get the base class of the API dependent classes (Material, Buffer, ...)
 #define IMPLEMENTATION(ClassName) IMPL_CONCAT(GPUIMPL_ID, ClassName)
-
 
 #ifdef WinPlatform
 
